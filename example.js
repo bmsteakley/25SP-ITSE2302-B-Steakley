42515e3d91ddaab34e28ceee26a81ebbd90f25df
bmsteakley <bmsteakley@mymail.tstc.edu>
2026-06-08 01:19:46 -0500

    Add files via upload

---
+++ example.js
@@ -0,0 +1,15 @@
+//declare variables
+let first_name = "Brianna";
+let last_name = "Steakley";
+let sentence = "This is a really awesome productivity website.";
+let theTruth = true;
+let num1 = 10;
+let num2 = 15;
+let num3 = 25;
+
+//outputs varibales and text to console
+console.log(first_name + " " + last_name); 
+console.log("The length of my sentence is: " + sentence.length + " characters long.");
+console.log("The sum of " + num1 + " and " + num2 + " is " + num3 + ".");
+console.log("The varaible theTruth is storing the value " + theTruth + ". The data type of theTruth is " + typeof(theTruth) + ".");
+
